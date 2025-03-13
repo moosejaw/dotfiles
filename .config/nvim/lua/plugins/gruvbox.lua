ffd690d26f811207dbbfaf5c5e5e44eec2f494a4
joshdemir <josh@kayenta.io>
2025-03-13 15:41:22 +0000

    Update nvim dotfiles
    A lot has changed with these, so keeping them up to date :bow:

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,13 @@
+vim.o.background = "light"
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
